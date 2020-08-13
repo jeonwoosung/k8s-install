1be31876e2ef3a0789c75ad56362b43b00460ba7
전우성 <jeonwoosung@gmail.com>
2020-08-13 10:49:53 +0900

    Update install.sh

--- rancher/install.sh
+++ rancher/install.sh
@@ -1,3 +1,6 @@
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.16.1/cert-manager.yaml
+kubectl create namespace cattle-system
+
 helm install rancher rancher-stable/rancher \
   --namespace cattle-system \
   --set hostname=rancher.oplab.co.kr
